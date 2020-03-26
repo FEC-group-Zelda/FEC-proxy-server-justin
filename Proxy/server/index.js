474d05aca59eca76f5d70938d0463cd6390eaf19
Justin Calvo <justin_calvo@hotmail.com>
2020-03-26 17:49:45 -0400

    Proxy server is rendering all modules properly and displaying in correct location

--- Proxy/server/index.js
+++ Proxy/server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const port = process.env.PORT || 3001;
+const cors = require('cors');
 
 const server = express();
 
 server.use(morgan('dev'));
 server.use(express.static(path.join(__dirname, '/../public')));
+server.use(cors());
 
 server.listen(port, () => {
   console.log(`proxy server listening on ${port}`);
